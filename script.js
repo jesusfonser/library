1605ddc6fc68da8a538ccab65150e851dbd02e15
jesusfonser <jesusfonseruma@gmail.com>
2025-02-24 14:07:27 +0100

    Programa lógica para añadir un libro

--- script.js
+++ script.js
@@ -13,18 +13,32 @@ function Libro(title, author, pages, isRead){
 */
 
 const library = [];
+let table = document.getElementsByTagName("table")[0];
 
-function Book(title, author, pages, isRead, summary){
+function Book(title, author, pages, summary, isRead){
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.isRead = isRead;
     this.summary = summary;
+    this.isRead = isRead;
 }
 
-function addNewBook(title, author, pages, isRead, summary){
-    let book = new Book(title, author, pages, isRead, summary);
+function addNewBook(title, author, pages, summary, isRead){
+    let book = new Book(title, author, pages, summary, isRead);
     library.push(book);
+    books2Page();
 }
 
-addNewBook("Lord of the Rings", "J.R.R. Tolkien", 295, false, "A book about hobbits");
+function books2Page(){
+    library.forEach(item =>{
+        let nrow = document.createElement("tr");
+        for (const key in item){
+            let ntd = document.createElement("td");
+            ntd.textContent = item[key];
+            nrow.appendChild(ntd);
+        }
+        table.appendChild(nrow);
+    })
+}
+
+addNewBook("Lord of the Rings", "J.R.R. Tolkien", 295,"A book about hobbits", false);
